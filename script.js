ac73accfd152a617ca1a25fb54b541d3a59b7a9d
pearl <pelroma22@src.pshs.edu.ph>
2025-01-23 08:34:35 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Declaring of variables
+
+const PI = 3.14;
+let radius = 6.378e6;
+let circumference = 2 * (PI * radius);
+let surfaceArea = 4 * (PI * (radius ** 2 ));
+let volume = (4/3) * (PI * (radius ** 3));
+
+// Printing of variables
+
+document.write('The radius of the sphere is ' + radius + '.');
+document.write('<br>');
+document.write('The circumference is ' + circumference) + '.';
+document.write('<br>');
+document.write('The surface area is ' + surfaceArea + '.');
+document.write('<br>');
+document.write('The volume is ' + volume + '.');
+
+
+
+
